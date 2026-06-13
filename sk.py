47848ab202b020cd5d447f3eaa53c65ba8dadc85
Sayali2610 <sayalikhedkar0@gmail.com>
2026-06-13 21:59:26 +0530

    Range function

---
+++ sk.py
@@ -0,0 +1,7 @@
+lis = []
+num = int(input("How many elements you want to enter:")) #10
+for i in range(1, num + 1):
+    element = int(input("enter exact number:"))
+    lis.append(element)
+print("origional list is:- ", lis)
+
